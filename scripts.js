775e94ba90eeeb68d18f7547913acc9e4b5cb327
Lucas  Silva Barbosa Oliveira <161037288+Lucssky@users.noreply.github.com>
2025-04-24 15:39:09 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -7,19 +7,15 @@ document.addEventListener("DOMContentLoaded", () => {
   let active = 0;
 
   function setSlider() {
-    // Remove o item e indicador ativos anteriores
+
     items.forEach(item => item.classList.remove('active'));
     dots.forEach(dot => dot.classList.remove('ativo'));
 
-    // Adiciona a classe no item e no dot ativo
     items[active].classList.add('active');
     dots[active].classList.add('ativo');
   }
 
-  // Inicializa
   setSlider();
-
-  // Adiciona evento de clique a todos os botões "Ver mais"
   let verMaisButtons = container.querySelectorAll('.ver-mais');
   verMaisButtons.forEach(button => {
     button.addEventListener('click', () => {
